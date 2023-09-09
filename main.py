c679ccadd1b8748f5c0480ebedaf2183e340a22c
zion928 <zion010928@gmail.com>
2023-09-09 18:16:51 +0900

    생성중

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-import discord  # 봇 토큰을 여기에 입력 
-token = "MTE0OTk3MzM3NTg3MjE1OTc3NA.Gp1Eyu.Dq-WP2iG-Z44Xwtvajr5TK6ReB64Tpo4ZODfFg"  
-# 봇 클라이언트 생성 
-client = discord.Client()  
+import discord
+from discord.ext import commands
 
-@client.event 
-async def on_ready():     
-    print(f'We have logged in as {client.user}')  
+import os
 
-@client.event 
-async def on_message(message):     
-    if message.author == client.user:         
-        return      
-    if message.content.startswith('!hello'):         
-        await message.channel.send('Hello!')  # 봇을 실행 
-    client.run(token)
+# 봇의 설정
+bot_token = os.environ.get("discord_token")
+bot_application_id = os.environ.get("discord_application_key")
+
+# 인텐트 설정
+intents = discord.Intents.default()
+intents.typing = False  # typing 이벤트 비활성화 (선택 사항)
+intents.presences = False  # presence 이벤트 비활성화 (선택 사항)
+intents.messages = True
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix="?",
+            intents=intents,
+            help_command=None,
+            application_id=bot_application_id  # 디스코드 봇의 application id를 입력하세요.
+        )
+
+    # 봇이 준비될 때 실행되는 이벤트 핸들러
+        async def on_ready(self):
+            print(f'We have logged in as {self.user}')
+
+    # 봇에 ?hello 명령어 추가
+    @commands.command(name='hello')
+    async def hello(ctx):
+        await ctx.send("Hello!")
+
+# 봇 실행
+bot = MyBot()
+bot.run(bot_token)  # 봇 토큰을 입력하세요.
